247d1b403054fb2d5495f8aeb33ac61d57e7b244
Jax Hoang <huynhhq209@gmail.com>
2026-01-13 20:21:52 +0700

    fix: remove fixed price data

--- src/problem2/src/data/index.ts
+++
@@ -1,3 +0,0 @@
-export { TOKEN_PRICES, getDeduplicatedPrices } from './tokenPrices';
-export { AVAILABLE_TOKENS, TOKEN_NAMES, getTokenName } from './tokenData';
-export type { AvailableToken } from './tokenData';
